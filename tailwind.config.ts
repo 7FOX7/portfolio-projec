bb58e8083fda2123436a1e0d9021ae49fb7aa86c
7FOX7 <kheeugene@gmail.com>
2024-10-28 16:23:20 -0400

    Added a spotlight component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,23 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'primary-color': "#00ffa7", 
+        'hover-color': "#02c280"
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
     },
   },
