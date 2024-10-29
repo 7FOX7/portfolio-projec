0f471b1645c12825ff7961f47bee078e2f40c92d
7FOX7 <kheeugene@gmail.com>
2024-10-29 15:58:51 -0400

    Added the content to the 'Main' component and modified the code accordingly

---
+++ app/components/main/main.tsx
@@ -0,0 +1,15 @@
+import { Spotlight } from "../ui/spotlight"
+import AboutMe from "./about-me"
+
+const Main = () => {
+   return (
+      <main>
+         <Spotlight fill="#00ffa7"/>
+         <AboutMe />
+      </main>      
+   )   
+}
+
+export default Main
+
+
