b027a338f400d001292c76721f1c98d7bec4c1f9
7FOX7 <kheeugene@gmail.com>
2024-11-12 21:10:30 -0500

    Added the content for the sections

--- app/components/main/contact-me.tsx
+++ app/components/main/contact-me.tsx
@@ -1,8 +1,15 @@
+import { Suspense } from "react"
+import { SectionContentSkeleton } from "../ui/skeletons"
+import InfoBlock from "./info-block"
 
 const ContactMe = () => {
    return (
-      <section className="px-5 mt-14">
-         <p>Hello from Contact Me section</p>
+      <section id="contact-me-section" className="px-5 mt-14">
+         <h2 className="mb-3 leading-none tracking-tight">Contact Me</h2>
+         <p className="mb-6 text-lg font-normal text-white lg:text-xl">Let&apos;s get in touch</p>
+         <Suspense fallback={<SectionContentSkeleton />}>      
+            <InfoBlock type="contact-me" />            
+         </Suspense>
       </section>
    )
 }
--- app/components/main/projects.tsx
+++ app/components/main/projects.tsx
@@ -1,8 +1,26 @@
+import { Suspense } from "react"
+import { SectionContentSkeleton } from "../ui/skeletons"
+import FilterBox from "./filter-box"
+import FilterLinks from "./filter-links"
+import { projectsSections } from "@/app/lib/data"
+import InfoBlock from "./info-block"
 
 const Projects = () => {
    return (
-      <section className="px-5 mt-14">
-         <p>Hello from Projects section</p>
+      <section id="projects-section" className="px-5 mt-14">
+         <h2 className="mb-3 leading-none tracking-tight">Projects</h2>
+         <p className="mb-6 text-lg font-normal text-white lg:text-xl">What have we got here</p>
+         <Suspense fallback={<SectionContentSkeleton />}>      
+            <section>
+               <div className="block md:hidden">
+                  <FilterBox sections={projectsSections} />
+               </div>           
+               <div className="hidden md:block">
+                  <FilterLinks sections={projectsSections} />
+               </div>
+               <InfoBlock type="projects" />
+            </section>
+         </Suspense>
       </section>
    )
 }
