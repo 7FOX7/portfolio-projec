87a8f6f4bb0775dc98843f5676e179570c4bf6c4
7FOX7 <kheeugene@gmail.com>
2024-11-12 21:11:05 -0500

    Added sections to the 'Main' component

--- app/components/main/main.tsx
+++ app/components/main/main.tsx
@@ -1,15 +1,17 @@
 import { Spotlight } from "../ui/spotlight"
 import AboutMe from "./about-me"
+import Projects from "./projects"
+import ContactMe from "./contact-me"
 
 const Main = () => {
    return (
       <main>
          <Spotlight fill="#00ffa7"/>
          <AboutMe />
+         <Projects />
+         <ContactMe />
       </main>      
    )   
 }
 
 export default Main
-
-
