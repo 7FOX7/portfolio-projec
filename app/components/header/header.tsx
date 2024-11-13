5dd9251e3a72f72f3a0684ebbc8e35bc9de17e8c
7FOX7 <kheeugene@gmail.com>
2024-11-12 20:28:16 -0500

    Adjusted values for the image and replaced routes with the extracted sections

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,32 +1,15 @@
 import Image from "next/image"
 import Link from "next/link"
+import { sections } from "@/app/lib/data"
 
-const routes = [
-  {
-    id: 'about-me', 
-    name: 'About Me', 
-    path: '/about-me'
-  }, 
-  {
-    id: 'projects', 
-    name: 'Projects', 
-    path: '/projects'
-  }, 
-  {
-    id: 'contact-me',
-    name: 'Contact Me', 
-    path: '/contact-me'
-  }
-]
-
-export function Header() {
+export default function Header() {
    return (
     <header>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <Image 
-              width={40}
+              width={32}
               height={40}
               src="/fox-logo.webp" className="h-8" alt="7FOX7 Logo" 
             />
@@ -34,11 +17,11 @@ export function Header() {
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 font-semibold text-primary-color">
               {
-                routes.map((route) => (
-                  <li key={route.id}>
-                    <Link href={route.path} 
+                sections.map((section) => (
+                  <li key={section.id}>
+                    <Link href={section.path} 
                       className="block py-2 px-3 rounded hover:hover-color md:border-0 md:hover:text-hover-color md:p-0">
-                      {route.name}
+                      {section.name}
                     </Link>
                   </li>
                 ))
@@ -49,4 +32,4 @@ export function Header() {
       </nav>     
     </header>
    )
-}
+} 
