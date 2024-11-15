87285f45f224ff6fe0f00958729236d7206d2b75
7FOX7 <kheeugene@gmail.com>
2024-11-15 17:54:46 -0500

    Added an additional font for larger devices

--- app/components/main/projects.tsx
+++ app/components/main/projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
    return (
       <section id="projects-section" className="px-5 mt-14">
          <h2 className="mb-3 leading-none tracking-tight">Projects</h2>
-         <p className="mb-6 text-lg font-normal text-white lg:text-xl">What have we got here</p>
+         <p className="mb-6 font-normal text-white text-xl lg:text-2xl">What have we got here</p>
          <Suspense fallback={<SectionContentSkeleton />}>      
             <section>
                <div className="block md:hidden">
