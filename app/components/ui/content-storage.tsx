27f371f27e74f6d78af58a344a4362ed172f9ac5
7FOX7 <kheeugene@gmail.com>
2024-11-15 17:55:21 -0500

    Added Accordion and Carousel to the 'Projects' section and adjusted the code accordingly

--- app/components/ui/content-storage.tsx
+++ app/components/ui/content-storage.tsx
@@ -1,8 +1,11 @@
 import { educationContent } from "@/app/lib/data"
 import { generalContent } from "@/app/lib/data"
 import { skillsContent } from "@/app/lib/data"
+import { bigProjectsContent } from "@/app/lib/data"
 import Image from "next/image"
-// import { sections } from "@/app/lib/data"
+import Carousel from "./carousel"
+import Link from "next/link"
+import Accordion from "./accordion"
 
 const ContentStorage = new Map() 
 
@@ -23,7 +26,7 @@ ContentStorage.set("general", () => {
 
 ContentStorage.set("skills", () => {
    return (
-      <div className="grid grid-cols-2 gap-y-4 md:grid-cols-3 justify-center items-center">
+      <div className="grid grid-cols-2 gap-y-4 lg:grid-cols-3 justify-center items-center">
          {skillsContent.map((skill) => (
          <div className="flex flex-col justify-center items-center" key={skill.id}>
             <Image
@@ -31,8 +34,9 @@ ContentStorage.set("skills", () => {
                width={skill.mobileWidth}
                height={skill.mobileHeight}
                alt={skill.alt}
+               draggable={false}
             />
-            <p className="font-medium text-xl">{skill.text}</p>
+            <p className="font-medium text-lg">{skill.text}</p>
          </div>
          ))}
       </div>
@@ -42,9 +46,127 @@ ContentStorage.set("skills", () => {
 
 ContentStorage.set("big-projects", () => {
    return (
-      <div>
-         <p>Hello from big projects</p>
-      </div>
+      <>
+         <div className="block md:hidden">
+            <Carousel 
+               items={bigProjectsContent} 
+               keys={bigProjectsContent.map(item => item.id)}
+               control="swipe"
+               allowGrab={false}
+               renderedItem={(bigProject) => (
+                  <div className="grid grid-cols-1">
+                     <div>
+                        <h3>{bigProject.titleName}</h3>
+                        <p>{bigProject.name}</p>
+                        <Carousel 
+                           items={bigProject.images} 
+                           keys={bigProjectsContent.map(item => item.id)}
+                           control="navigation"
+                           allowGrab={false}
+                           renderedItem={(img) => (
+                              <Image 
+                                 src={img.src} 
+                                 alt={img.alt} 
+                                 width={img.mobileWidth}
+                                 height={img.mobileHeight}
+                              />
+                           )} 
+                        />
+                     </div>
+                     <div>
+                        <div>
+                           <h3>{bigProject.titleDescription}</h3>
+                           <Accordion 
+                              wrapperKey={`accordion-wrapper-${bigProject.id}`} 
+                              values={[
+                              {
+                                 key: `accordion-inner-${bigProject.id}`, 
+                                 heading: "Description", 
+                                 content: 
+                                    <div className="block">
+                                       <p>{bigProject.description}</p>
+                                    </div>
+                              }, 
+                           ]}/>
+                        </div>
+                        <div>
+                           <h3>{bigProject.titleTechnologies}</h3>
+                           <div>
+                              {bigProject.technologies.map(tech => (
+                                 <div key={tech.id}>
+                                    <p>{tech.name}</p>
+                                 </div>
+                              ))}
+                           </div>
+                        </div>
+                        <div>
+                           <h3>{bigProject.titleDemonstration}</h3>
+                           <div className="grid grid-cols-2">
+                              <Link href={bigProject.codeLink.href} className="text-primary-color underline text-lg md:text-xl">{bigProject.codeLink.text}</Link>
+                              <Link href={bigProject.liveLink.href} className="text-primary-color underline text-lg md:text-xl">{bigProject.liveLink.text}</Link>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               )} 
+            />
+         </div>
+         <div className="hidden md:block">
+            <Carousel 
+               items={bigProjectsContent} 
+               keys={bigProjectsContent.map(item => item.id)}
+               control="navigation"
+               allowGrab={false}
+               renderedItem={(bigProject) => (
+                  <div className="grid grid-cols-2">
+                     <div>
+                        <h3>{bigProject.titleName}</h3>
+                        <p>{bigProject.name}</p>
+                        <Carousel 
+                           items={bigProject.images} 
+                           keys={bigProjectsContent.map(item => item.id)}
+                           control="pagination"
+                           allowGrab={true}
+                           renderedItem={(img) => (
+                              <Image 
+                                 src={img.src} 
+                                 alt={img.alt} 
+                                 width={img.desktopWidth}
+                                 height={img.desktopHeight}
+                              />
+                           )} 
+                        />
+                     </div>
+                     <div>
+                        <div>
+                           <h3>{bigProject.titleDescription}</h3>
+                           <div className="block">
+                              <p>{bigProject.description}</p>
+                           </div>
+                        </div>
+                        <div>
+                           <h3>{bigProject.titleTechnologies}</h3>
+                           <div>
+                              {bigProject.technologies.map(tech => (
+                                 <div key={tech.id}>
+                                    <p>{tech.name}</p>
+                                 </div>
+                              ))}
+                           </div>
+                        </div>
+                        <div>
+                           <h3>{bigProject.titleDemonstration}</h3>
+                           <div className="grid grid-cols-2">
+                              <Link href={bigProject.codeLink.href} className="text-primary-color underline text-lg md:text-xl">{bigProject.codeLink.text}</Link>
+                              <Link href={bigProject.liveLink.href} className="text-primary-color underline text-lg md:text-xl">{bigProject.liveLink.text}</Link>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               )} 
+            />
+         </div>
+      </>
    )
 })
 
