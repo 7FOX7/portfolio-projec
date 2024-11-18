512040d4630386e63ebd569bb3e6fd1970540d98
7FOX7 <kheeugene@gmail.com>
2024-11-18 03:26:17 -0500

    Adjusted padding

--- app/components/main/content-wrapper.tsx
+++ app/components/main/content-wrapper.tsx
@@ -1,7 +1,7 @@
 const ContentWrapper = ({children}: {children: React.ReactNode}) => {
    return (
       <div className="relative flex items-center justify-center p-4 bg-primary-color text-white text-lg">
-         <div className="relative w-full bg-black p-6">
+         <div className="relative w-full bg-black px-7 py-8">
             {children}
          </div>
       </div>
