16e6276cb07e1e59d530b907ab31212c39c5bdfd
7FOX7 <kheeugene@gmail.com>
2024-11-18 03:40:13 -0500

    Code cleanup

--- app/components/main/filter-box.tsx
+++ app/components/main/filter-box.tsx
@@ -9,13 +9,11 @@ import { _aboutMeSections } from "@/app/lib/data"
 import { _projectsSections } from "@/app/lib/data"
 
 const FilterBox = ({sections}: {sections: Sections}) => {
-   console.log('filter box is recreated')
    const searchParams = useSearchParams()
    const pathname = usePathname()
    const router = useRouter()
 
    useEffect(() => {
-      console.log('useEffect is executed')
       router.replace('/?about-me=education&projects=big-projects')   // navigate to the default path
    }, [router])
 
@@ -48,32 +46,3 @@ const FilterBox = ({sections}: {sections: Sections}) => {
 }
 
 export default FilterBox
-
-/*
-   does it make sense to use 'useCallback' within a component whose parent is rerendering? Assume we have the code as follows: 
-
-
-   <Home>
-      <Sales />
-   </Home>
-
-   const Sales = () => {
-      ....
-      const router = useRouter()
-      const myFunc = useCallback(() => {
-         router.replace('/another/router')
-      }, [])
-
-      return (
-         // some content here
-      )
-   }
-
-   const Home = () => {
-      console.log('rerendered')     // will be logged each time we change the route
-   }
-
-
-   now, is the 'myFunc' will be rerendered? I think that yes, it will be rerendered, simply because we recreate the entire component tree, the parent is recreated, and the child component is recreated, too, unless you wrap 
-   your 'Sales' into 'memo' (but even this is not going to work since by changing the router, all the components are rerendered). The virtual DOM is recreated, and a new DOM is created every time
-*/
