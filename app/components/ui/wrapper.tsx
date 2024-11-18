8db0b37db2d91961ec226fd0d401367ac25d9569
Evgenii Khe <ekhe1@ncstudents.niagaracollege.ca>
2024-11-18 15:03:16 -0500

    Added a min height to the wrapper

--- app/components/ui/wrapper.tsx
+++ app/components/ui/wrapper.tsx
@@ -1,6 +1,6 @@
 const Wrapper = ({children}: {children: React.ReactNode}) => {
    return (
-      <div className="px-5 md:px-10 w-full min-w-96 max-w-7xl justify-self-center">
+      <div className="px-5 md:px-10 w-full min-w-96 max-w-7xl justify-self-center min-h-screen">
          {children}
       </div>
    )
